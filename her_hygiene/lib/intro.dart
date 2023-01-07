664b90ce9558876b4d58e79082945ce12521bf6b
Bazgha19 <sbs019190@gmail.com>
2023-01-07 20:26:04 +0530

    Update herhygiene

---
+++ her_hygiene/lib/intro.dart
@@ -0,0 +1,268 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:her_hygiene/home_screen.dart';
+import 'package:her_hygiene/main.dart';
+
+class Intro extends StatefulWidget {
+  @override
+  _IntroState createState() => _IntroState();
+}
+
+class _IntroState extends State<Intro> {
+  final int _numPages = 3;
+  final PageController _pageController = PageController(initialPage: 0);
+  int _currentPage = 0;
+
+  List<Widget> _buildPageIndicator() {
+    List<Widget> list = [];
+    for (int i = 0; i < _numPages; i++) {
+      list.add(i == _currentPage ? _indicator(true) : _indicator(false));
+    }
+    return list;
+  }
+
+  Widget _indicator(bool isActive) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 150),
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 8.0,
+      width: isActive ? 24.0 : 16.0,
+      decoration: BoxDecoration(
+        color: isActive ? Colors.white : Colors.pink.shade50,
+        borderRadius: BorderRadius.all(
+          Radius.circular(12),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.1, 0.4, 0.7, 0.9],
+              colors: [
+                Colors.pink.shade100,
+                Colors.pink.shade100,
+                Colors.pink.shade100,
+                Colors.pink.shade100,
+              ],
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => Home()));
+                    },
+                    child: Text(
+                      'Skip',
+                      style: TextStyle(
+                        color: Color.fromRGBO(179, 68, 98, 1),
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 600.0,
+                  child: PageView(
+                    physics: ClampingScrollPhysics(),
+                    controller: _pageController,
+                    onPageChanged: (int page) {
+                      setState(() {
+                        _currentPage = page;
+                      });
+                    },
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(40.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Center(
+                              child: Image(
+                                image: AssetImage(
+                                  'assets/intro1.png',
+                                ),
+                                height: 300.0,
+                                width: 300.0,
+                              ),
+                            ),
+                            SizedBox(height: 30.0),
+                            Text(
+                              'Hygiene In Your Hands',
+                              style: TextStyle(
+                                  fontSize: 40.0,
+                                  color: Color.fromRGBO(179, 68, 98, 1)),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(height: 15.0),
+                            Text(
+                              'The only app you will ever need to know about your hygiene',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: Color.fromRGBO(195, 91, 118, 1),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(40.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Center(
+                              child: Image(
+                                image: AssetImage(
+                                  'assets/track.png',
+                                ),
+                                height: 300.0,
+                                width: 300.0,
+                              ),
+                            ),
+                            SizedBox(height: 30.0),
+                            Text(
+                              'Track Your Hygiene',
+                              style: TextStyle(
+                                  fontSize: 40.0,
+                                  color: Color.fromRGBO(179, 68, 98, 1)),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(height: 15.0),
+                            Text(
+                              'Quizes helps you in analyzing your hygiene',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: Color.fromRGBO(195, 91, 118, 1),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(40.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Center(
+                              child: Image(
+                                image: AssetImage(
+                                  'assets/start.png',
+                                ),
+                                height: 300.0,
+                                width: 300.0,
+                              ),
+                            ),
+                            SizedBox(height: 30.0),
+                            Text(
+                              'Enter In The World Of HerHygiene',
+                              style: TextStyle(
+                                  fontSize: 40.0,
+                                  color: Color.fromRGBO(179, 68, 98, 1)),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(height: 15.0),
+                            Text(
+                              'It take less then a minute to enter and enjoy HerHygiene',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: Color.fromRGBO(195, 91, 118, 1),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: _buildPageIndicator(),
+                ),
+                _currentPage != _numPages - 1
+                    ? Expanded(
+                        child: Align(
+                          alignment: FractionalOffset.bottomRight,
+                          child: TextButton(
+                            onPressed: () {
+                              print('Next');
+                              _pageController.nextPage(
+                                duration: Duration(milliseconds: 500),
+                                curve: Curves.ease,
+                              );
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text(
+                                  'Next',
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(179, 68, 98, 1),
+                                    fontSize: 22.0,
+                                  ),
+                                ),
+                                SizedBox(width: 10.0),
+                                Icon(
+                                  Icons.arrow_forward,
+                                  color: Color.fromRGBO(179, 68, 98, 1),
+                                  size: 30.0,
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    : Text(''),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomSheet: _currentPage == _numPages - 1
+          ? Container(
+              height: 100.0,
+              width: double.infinity,
+              color: Colors.white,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => Home()));
+                },
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 30.0),
+                    child: Text(
+                      'Get Started',
+                      style: TextStyle(
+                        color: Color.fromRGBO(179, 68, 98, 1),
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          : Text(''),
+    );
+  }
+}
--- her_hygiene/lib/main.dart
+++ her_hygiene/lib/main.dart
@@ -1,31 +1,78 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:her_hygiene/Contraceptive%20Health%20Test/Quiz_screen.dart';
 import 'package:her_hygiene/Menstrual%20Health%20Test/self_test.dart';
+import 'package:her_hygiene/intro.dart';
+import 'package:her_hygiene/products/product.dart';
 import 'package:her_hygiene/quiz_selection.dart';
 import './home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AnimatedSplashScreen(
+        splash: Image.asset('assets/HerLogo.png'),
+        backgroundColor: Colors.pink.shade100,
+        splashIconSize: 80.0,
+        splashTransition: SplashTransition.rotationTransition,
+        duration: 1000,
+        nextScreen: Intro(),
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int onClick = 0;
+
+  final List pageToNavigate = [
+    HomeScreen(),
+    Product(),
+    QuizScreen(),
+    SelfTestScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: 'quiz_option',
-      routes: {
-        'quiz_option': (context) => const QuizOption(),
-        'home_screen': (context) => const HomeScreen(),
-        'quiz_screen': (context) =>
-            const QuizScreen(), //screen  fro contraceptive health
-        'self_test': (context) =>
-            const SelfTestScreen(), //screen for Menstrual health quiz
-      },
-      /* home: SelfTestScreen(), */
+      home: Scaffold(
+        body: pageToNavigate[onClick],
+        backgroundColor: Colors.white,
+        bottomNavigationBar: CurvedNavigationBar(
+          animationDuration: Duration(milliseconds: 150),
+          backgroundColor: Colors.white,
+          color: Colors.pink.shade50,
+          onTap: (index) {
+            setState(() {
+              onClick = index;
+              print(onClick);
+            });
+          },
+          items: <Widget>[
+            Icon(Icons.home),
+            Icon(Icons.water_drop),
+            Icon(Icons.medical_information),
+            Icon(Icons.people),
+          ],
+        ),
+      ),
     );
   }
 }
